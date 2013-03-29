84b29ae989bc6f6ee0f1873ba99cd97c39203e42
jteng <tengj6@yahoo.com>
2013-03-29 16:58:15 -0400

    Chinese content

---
+++ chccc/WebContent/zh/content/home_sidebar.php
@@ -0,0 +1,13 @@
+<div id="sidebar_container">
+        <div class="sidebar">
+          <h3>教會消息</h3>
+		  <?php include "$_SERVER[DOCUMENT_ROOT]/zh/content/news_summary.php"; ?>
+        </div>
+        <div class="sidebar">
+          <h3>主日影音</h3>
+          <table>
+            <tr><td>于慕潔牧師</td><td>03/24/2013</td><td><a href="#">看哪！神的羔羊</a></td><td>pdf</td></tr>
+			<tr><td>于慕潔牧師</td><td>03/27/2013</td><td><a href="#">將心歸主, 同心愛主</a></td><td>pdf</td></tr>
+          </table>
+        </div>
+</div>
