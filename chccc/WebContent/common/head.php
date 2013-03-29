339f00092050981a70521a1ba5a864f63608a207
jteng <tengj6@yahoo.com>
2013-03-29 18:48:14 -0400

    add seo keywords

--- chccc/WebContent/common/head.php
+++ chccc/WebContent/common/head.php
@@ -1,6 +1,6 @@
   <title>Cherry Hill Chinese Christian Church</title>
   <meta name="description" content="website description" />
-  <meta name="keywords" content="website keywords, website keywords" />
+  <meta name="keywords" content="Cherry Hill Chinese Christian Church, CHCCC，于慕潔" />
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
   <link rel="stylesheet" type="text/css" href="/css/style.css" />
   <link href='http://fonts.googleapis.com/css?family=Berkshire+Swash' rel='stylesheet' type='text/css'>
