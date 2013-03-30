241fd54968d415902116788813f4e807e3d645fc
jteng <tengj6@yahoo.com>
2013-03-30 15:31:01 -0400

    finishing up topnav links

--- chccc/WebContent/zh/aboutus/aboutus_leftnav.php
+++ chccc/WebContent/zh/aboutus/aboutus_leftnav.php
@@ -2,7 +2,7 @@
         <div class="leftnav">
         	<h3>簡介</h3>
 	        <ul>
-		        <li><a href="link">教會信仰</a></li>
+		        <li><a href="/zh/aboutus/statement.php">教會信仰</a></li>
 		        <li><a href="link">教會歷史</a></li>
 		        <li><a href="link">教會會眾</a></li>
 		        <li><a href="link">浸禮與會員</a></li>
--- chccc/WebContent/zh/common/header.php
+++ chccc/WebContent/zh/common/header.php
@@ -29,40 +29,40 @@ function switchLanguage($target){
         <div id="menu_container">
           <ul class="sf-menu" id="nav">
             <li><a href="/zh/index.php">主頁</a></li>
-            <li><a href="examples.html">簡介</a>
+            <li><a href="/zh/aboutus/statement.php">簡介</a>
                <ul>
                 <li><a href="/zh/aboutus/statement.php">教會信仰</a></li>
-                <li><a href="#">教會歷史</a></li>
-                <li><a href="#">教會會眾</a></li>
-                <li><a href="#">浸禮與會員</a></li>
-                <li><a href="#">地圖</a></li>
-                <li><a href="#">聯絡我們</a></li>
+                <li><a href="/zh/aboutus/history.php">教會歷史</a></li>
+                <li><a href="/zh/aboutus/congregation.php">教會會眾</a></li>
+                <li><a href="/zh/aboutus/baptism_membership.php">浸禮與會員</a></li>
+                <li><a href="/zh/aboutus/map.php">地圖</a></li>
+                <li><a href="/zh/aboutus/contact_us.php">聯絡我們</a></li>
               </ul>	
             </li>
-            <li><a href="page.html">崇拜</a>
+            <li><a href="/zh/worship/children_worship.php">崇拜</a>
               <ul>
-                <li><a href="#">兒童崇拜</a></li>
-                <li><a href="#">詩歌</a></li>
-                <li><a href="#">網絡信息</a></li>
-                <li><a href="#">崇拜時間</a></li>
+                <li><a href="/zh/worship/children_worship.php">兒童崇拜</a></li>
+                <li><a href="/zh/worship/hymns.php">詩歌</a></li>
+                <li><a href="/zh/worship/messages.php">網絡信息</a></li>
+                <li><a href="/zh/worship/worship_time.php">崇拜時間</a></li>
               </ul>	
             </li>
-            <li><a href="another_page.html">進修</a>
+            <li><a href="/zh/sundayschool/children_sunday_school.php">進修</a>
               <ul>
-                <li><a href="#">兒童主日學</a></li>
-                <li><a href="#">成人主日學</a></li>
+                <li><a href="/zh/sundayschool/children_sunday_school.php">兒童主日學</a></li>
+                <li><a href="/zh/sundayschool/adult_sunday_school.php">成人主日學</a></li>
               </ul>	
             </li>
-            <li><a href="#">我們的社區</a>
+            <li><a href="/zh/community/community_ministry.php">我們的社區</a>
               <ul>
-                <li><a href="#">社區事工</a></li>
-                <li><a href="#">兒童事工</a></li>
-                <li><a href="#">青少年事工</a></li>
-                <li><a href="#">英文堂</a></li>
-                <li><a href="#">國語堂</a></li>
+                <li><a href="/zh/community/community_ministry.php">社區事工</a></li>
+                <li><a href="/zh/community/children_ministry.php">兒童事工</a></li>
+                <li><a href="/zh/community/youth_ministry.php">青少年事工</a></li>
+                <li><a href="/zh/community/english_ministry.php">英文堂</a></li>
+                <li><a href="/zh/community/mandarin_ministry.php">國語堂</a></li>
               </ul>
             </li>
-            <li><a href="events.php">最新活動</a></li>
+            <li><a href="/events.php">最新活動</a></li>
             <li><a href="<?php switchLanguage('en'); ?>">English</a></li>
           </ul>
         </div>
