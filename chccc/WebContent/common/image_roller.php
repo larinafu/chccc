a57bc9035f245e5ef2a3b9ba4f70140e75134f7d
jteng <tengj6@yahoo.com>
2013-04-08 19:25:41 -0400

    resize images on the fly if needed.

--- chccc/WebContent/common/image_roller.php
+++ chccc/WebContent/common/image_roller.php
@@ -1,4 +1,6 @@
 <?php
+include('SimpleImage.php');
+
 if ($handle = opendir("$_SERVER[DOCUMENT_ROOT]/images/roller/")) {
     echo "<div id='makeMeScrollable'>";
     
@@ -7,10 +9,22 @@ if ($handle = opendir("$_SERVER[DOCUMENT_ROOT]/images/roller/")) {
     while (false !== ($entry = readdir($handle))) {
     	if("."==$entry || ".."==$entry)continue;
         echo "<img src='/images/roller/$entry' />";
+        $image_info=getimagesize("$_SERVER[DOCUMENT_ROOT]/images/roller/$entry");
+        if($image_info[1]>330){
+        	$image = new SimpleImage();
+        	$image->load("$_SERVER[DOCUMENT_ROOT]/images/roller/$entry");
+        	$image->resizeToHeight(330);
+  			$image->save("$_SERVER[DOCUMENT_ROOT]/images/roller/$entry");
+        }
     }
 
 	echo "</div>";
     closedir($handle);
+    
+}
+
+function resize($filename){
+	
 }
 ?>
 
