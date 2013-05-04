31a0702ea726d435db263105b57cea764912c909
jteng <tengj6@yahoo.com>
2013-05-03 22:49:55 -0400

    neighbour church content

--- chccc/WebContent/zh/content/neighbour_churches_content.php
+++ chccc/WebContent/zh/content/neighbour_churches_content.php
@@ -8,10 +8,11 @@
 <li class="left">若歌教會</li>
 </ul>
 -->
+<li>周邊教會</li>
 <table style="width:80%">
-<tr><td>若歌教會</td><td>威明頓主恩堂</td></tr>
-<tr><td>費城中華基督教會</td><td>信望愛華人教會</td></tr>
-<tr><td>康州哈城華人浸信會</td><td>美門華人基督教會</td></tr>
-<tr><td>費城三一華人基督教會</td><td>$nbsp;</td></tr>
+<tr><td><a href="http://www.rccc.org">若歌教會</a></td><td><a href="http://www.wcec-home.org/">威明頓主恩堂</a></td></tr>
+<tr><td><a href="http://msm.cccnc.org/">費城中華基督教會</a></td><td><a href="http://www.fhlcc.org/">信望愛華人教會</a></td></tr>
+<tr><td><a href="http://www.cbcgh.org/index.aspx">康州哈城華人浸信會</a></td><td><a href="http://www.mccc.org/">美門華人基督教會</a></td></tr>
+<tr><td><a href="http://www.tccgp.org/wp/">費城三一華人基督教會</a></td><td><a href="http://www.wccec.org/newWCCEC/Version1/welcome.php">威徹斯特主恩堂</a></td></tr>
 </table>
 </div> 
