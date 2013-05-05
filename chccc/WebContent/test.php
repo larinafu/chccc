78dfdb0b6467a5106529cd38dc98fbe92997648d
jteng <tengj6@yahoo.com>
2013-05-04 22:06:55 -0400

    add missed columns and some test data

--- chccc/WebContent/test.php
+++ chccc/WebContent/test.php
@@ -1,33 +1,3 @@
 <?php
-/*
- * Created on Mar 9, 2013
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- * test
- */
- echo "server docu root: $_SERVER[DOCUMENT_ROOT]";
- 
-if ($handle = opendir("$_SERVER[DOCUMENT_ROOT]/images")) {
-    echo "Directory handle: $handle\n";
-    echo "Entries:\n";
-
-    /* This is the correct way to loop over the directory. */
-    while (false !== ($entry = readdir($handle))) {
-        echo "$entry\n<br>";
-    }
-
-    /* This is the WRONG way to loop over the directory. 
-    while ($entry = readdir($handle)) {
-        echo "$entry\n";
-    }
-    */
-
-    closedir($handle);
-}
-
-echo "do"
-
-
-
+phpinfo();
 ?>
--- chccc/sql/fellowship.sql
+++ chccc/sql/fellowship.sql
@@ -2,12 +2,14 @@ create table ch_group(
 	group_id MEDIUMINT NOT NULL AUTO_INCREMENT,
 	group_name varchar(50) NOT NULL,
 	group_description varchar(1000),
+	sort_order SMALLINT,
 	PRIMARY KEY(group_id)
 );
 
 create table ch_group_photo (
 	photo_id MEDIUMINT NOT NULL AUTO_INCREMENT,
 	photo_path varchar (100) NOT NULL,
+	photo_description varchar(1000),
 	group_id MEDIUMINT  NOT NULL,
 	PRIMARY KEY(photo_id)
 );
