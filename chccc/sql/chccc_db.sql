e25e8cf03086da66dbdd42f42ab5b45563dca2cc
qianjunsayhi <qianjunsayhi@yahoo.com>
2013-05-11 08:44:25 -0700

    Updated for English Section

--- chccc/sql/chccc_db.sql
+++ chccc/sql/chccc_db.sql
@@ -10,6 +10,7 @@ create table ch_message(
 	message_id MEDIUMINT NOT NULL AUTO_INCREMENT,
 	message_date date NOT NULL,
 	speaker varchar(120) NOT NULL,
+	speaker_title varchar(120) NOT NULL,
 	message_title varchar(500) NOT NULL,
 	speaker_en varchar(120),
 	message_title_en varchar(500),
@@ -23,6 +24,25 @@ create table ch_message(
 );
 create index idx_ch_message_date on ch_message(message_date);
 create index idx_ch_message_speaker on ch_message(speaker);
+
+create table ch_message_en(
+	message_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	message_date date NOT NULL,
+	speaker varchar(120) NOT NULL,
+	speaker_title varchar(120) NOT NULL,
+	message_title varchar(500) NOT NULL,
+	speaker_zh varchar(120),
+	message_title_zh varchar(500),
+	message_audio_file_name varchar(200),
+	message_pdf_file_name varchar(200),
+	message_video_file_name varchar(200),
+	bible_verses varchar(1000),
+	bible_verses_en varchar(1000),
+	published bool DEFAULT TRUE,
+	PRIMARY KEY(message_id)
+);
+create index idx_ch_message_date on ch_message_en(message_date);
+create index idx_ch_message_speaker on ch_message_en(speaker);
 
 create table ch_news(
 	news_id MEDIUMINT NOT NULL AUTO_INCREMENT,
@@ -40,7 +60,6 @@ create index idx_ch_news_date on ch_news(news_date);
 create table ch_music(
 	music_id MEDIUMINT NOT NULL AUTO_INCREMENT,
 	music_date date NOT NULL,
-	music_speaker varchar(100) NOT NULL,
 	music_name varchar(500) NOT NULL,
 	music_name_en varchar(500),
 	music_audio_file_name varchar(200),
@@ -51,4 +70,21 @@ create table ch_music(
 );
 create index idx_ch_music_date on ch_music(music_date);
 
-
+create table ch_message_en(
+	message_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	message_date date NOT NULL,
+	speaker varchar(120) NOT NULL,
+	speaker_title varchar(120) NOT NULL,
+	message_title varchar(500) NOT NULL,
+	speaker_zh varchar(120),
+	message_title_zh varchar(500),
+	message_audio_file_name varchar(200),
+	message_pdf_file_name varchar(200),
+	message_video_file_name varchar(200),
+	bible_verses varchar(1000),
+	bible_verses_zh varchar(1000),
+	published bool DEFAULT TRUE,
+	PRIMARY KEY(message_id)
+);
+create index idx_ch_message_date on ch_message_en(message_date);
+create index idx_ch_message_speaker on ch_message_en(speaker);
--- chccc/sql/fellowship.sql
+++ chccc/sql/fellowship.sql
@@ -1,7 +1,9 @@
 create table ch_group(
 	group_id MEDIUMINT NOT NULL AUTO_INCREMENT,
 	group_name varchar(50) NOT NULL,
+	group_name_en varchar(50) NOT NULL,
 	group_description varchar(1000),
+	group_description_en varchar(1000),
 	sort_order SMALLINT,
 	PRIMARY KEY(group_id)
 );
@@ -10,6 +12,7 @@ create table ch_group_photo (
 	photo_id MEDIUMINT NOT NULL AUTO_INCREMENT,
 	photo_path varchar (100) NOT NULL,
 	photo_description varchar(1000),
-	group_id MEDIUMINT  NOT NULL,
+	photo_description_en varchar(1000),
+	group_id MEDIUMINT NOT NULL,
 	PRIMARY KEY(photo_id)
 );
