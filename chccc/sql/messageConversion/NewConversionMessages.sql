eb8293f9437732d7a44a9bea4da184b184976d3a
yinghuihu <yinghuihu@gmail.com>
2013-06-09 06:43:44 -0700

    new messages

---
+++ chccc/sql/messageConversion/NewConversionMessages.sql
@@ -0,0 +1,62 @@
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('于慕潔', '牧師', '你們必須重生', '2013-06-02', '06022013.wma', '約翰福音3：1－16');
+
+
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('于慕潔', '牧師', '禱告你在暗中的父', '2013-05-26', '05262013.wma', '馬太福音6：5－13 馬可福音1：35 路加福音6：12 約翰福音12：28');
+
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('于慕潔', '牧師', '住在至高者的隱秘處', '2013-05-19', '05192013.wma', '詩篇 第九十一篇 ');
+
+
+
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('于慕潔', '牧師', '祈禱的母親', '2013-05-12', '05122013.wma', '撒上1：1－24 路加福音 1：5－7,13-17');
+
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('David Chang', '', '馬其頓的呼聲', '2013-05-05', '05052013.wma', '使徒行傳 16：6－10');
+
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('于慕潔', '牧師', '尋求神', '2013-04-28', '04282013.wma', '詩篇 63');
+
+
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('梁中杰', '牧師', '戰勝試探', '2013-04-21', '04212013.wma', '馬太福音 4：1－11');
+
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('尤其方', '長老', '跟隨之路', '2013-04-14', '04142013.wma', '馬太福音 16：13－17，21－23 約翰福音 21：18－22');
+
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('于慕潔', '牧師', '選擇那上好的福分', '2013-04-07', '04072013.wma', '路加福音 10：38－42 約翰福音 12：1－3，7－8');
+
+
+
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('于慕潔', '牧師', '除他以外別無拯救', '2013-03-31', '03312013.wma', '使徒行傳 4：1－12 約翰福音 11：25－26');
+
+
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('于慕潔', '牧師', '他不在這裏', '2013-03-31', 'easter_2013_1.mp3', '馬太福音 28：1－7， 16－20');
+
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('于慕潔', '牧師', '看哪！神的羔羊', '2013-03-24', '03242013.wma', '約翰福音 1：29； 12：12－16 啟示錄 7：9－10，17；17：14');
+
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('于慕潔', '牧師', '將心歸主, 同心愛主', '2013-03-17', '03172013.wma', '約翰福音 21：15－19 箴言 23:26');
+
+INSERT INTO ch_message 
+(speaker, speaker_title, message_title, message_date, message_audio_file_name, bible_verses) VALUES
+('于慕潔', '牧師', '一同追求，作合于主用的貴重器皿', '2013-03-10', '03102013.wma', '提摩太後書 2：19－23');
