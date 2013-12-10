52c85f4485afd73065a15765c649a111d395a834
qianjunsayhi <qianjunsayhi@yahoo.com>
2013-12-10 11:46:06 -0800

    To point to the right location

--- chccc/WebContent/zh/content/year_books_content.php
+++ chccc/WebContent/zh/content/year_books_content.php
@@ -1,9 +1,14 @@
 <div class="subcontent">
 <li>年刊一覽</li>
 <table style="width:80%">
-<tr><td><a href="http://www.rccc.org">十周年慶(2012.10)</a></td><td><a href="http://www.wcec-home.org/">九周年慶(2011.10)</a></td></tr>
-<tr><td><a href="http://msm.cccnc.org/">八周年慶(2010.10)</a></td><td><a href="http://www.fhlcc.org/">七周年慶(2009.10)</a></td></tr>
-<tr><td><a href="http://www.cbcgh.org/index.aspx">六周年慶(2008.11)</a></td><td><a href="http://www.mccc.org/">五周年慶(2007.10)</a></td></tr>
-<tr><td><a href="http://www.tccgp.org/wp/">四週年慶(2006.10)</a></td><td><a href="http://www.wccec.org/newWCCEC/Version1/welcome.php">周年特刊(2003.10)</a></td></tr>
+<tr><td><a href="http://www.cherryhillccc.org/forms/11thAnniversary.pdf">十周年慶(2013.10)</a></td></tr>
+<tr><td><a href="http://www.cherryhillccc.org/forms/10thAnniversary.pdf">十周年慶(2012.10)</a></td></tr>
+<tr><td><a href="http://www.cherryhillccc.org/forms/9thAnniversary.pdf">九周年慶(2011.10)</a></td></tr>
+<tr><td><a href="http://www.cherryhillccc.org/forms/8thAnniversary.pdf">八周年慶(2010.10)</a></td></tr>
+<tr><td><a href="http://www.cherryhillccc.org/forms/7thAnniversary.pdf">七周年慶(2009.10)</a></td></tr>
+<tr><td><a href="http://www.cherryhillccc.org/forms/6thAnniversary.pdf">六周年慶(2008.11)</a></td></tr>
+<tr><td><a href="http://www.cherryhillccc.org/forms/paper5.doc">五周年慶(2007.10)</a></td></tr>
+<tr><td><a href="http://www.cherryhillccc.org/forms/Paper4.doc">四週年慶(2006.10)</a></td></tr>
+<tr><td><a href="http://www.cherryhillccc.org/forms/Anniversary.doc">周年特刊(2003.10)</a></td></tr>
 </table>
 </div> 
