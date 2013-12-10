15ed659c11995c3a90785b28dfbb6d1e9939628a
qianjunsayhi <qianjunsayhi@yahoo.com>
2013-12-10 11:58:10 -0800

    Added path for English Admin

--- chccc/WebContent/common/db_conn.php
+++ chccc/WebContent/common/db_conn.php
@@ -4,5 +4,7 @@ $password="53chccc2004";//"Uvfy5RYduLj8fEMa"; //
 $database="chccc";
 $db_host="localhost"; //;//mysql
 $audio_library="http://www.cherryhillccc.org/ChineseSundayMessage/";
-$pdf_library="/ChineseSundayMessage/PDF/";		
+$pdf_library="/ChineseSundayMessage/PDF/";	
+$audio_library_en="http://www.cherryhillccc.org/ChineseSundayMessage/EnglishService/";
+$pdf_library_en="/ChineseSundayMessage/EnglishService/PDF/";		
 ?>
