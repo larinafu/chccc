f05ba2924d7fa5da68150f5f99a6c18f992d4c2f
yinghuihu <yinghuihu@gmail.com>
2014-05-26 20:25:30 -0400

    2014 Summer Sunday school and library related changes, all checked in.

--- chccc/WebContent/zh/education/leftnav.php
+++ chccc/WebContent/zh/education/leftnav.php
@@ -4,12 +4,6 @@
 	        <ul>
 		        <li><a href="/zh/education/introduction.php">教育事工簡介</a></li>
                 <li><a href="/zh/education/trainning.php">同工培訓</a></li>
-                <!--<li><a href="#" style="text-decoration:none;">2014春季主日學</a>
-                	<ul>
-		                <li style="padding: 0 0 4px 20px;"><a href="/zh/education/adult_school.php">成人主日學</a></li>
-		                <li style="padding: 0 0 4px 20px;"><a href="/zh/education/children_school.php">兒童主日學</a></li>
-		            </ul>
-                </li>-->
                 <li><a href="#" style="text-decoration:none;">2014夏季主日學</a>
                 	<ul>
 		                <li style="padding: 0 0 4px 20px;"><a href="/zh/education/adult_school_june.php">成人主日學</a></li>
@@ -19,7 +13,7 @@
                 <li><a href="#" style="text-decoration:none;">圖書館</a>
                 	<ul>
 		                <li style="padding: 0 0 4px 20px;"><a href="/zh/education/library_rule.php">借閱規則</a></li>
-		                <li style="padding: 0 0 4px 20px;"><a href="/zh/education/library_catalog_2014-04-05_toweb.xlsx">書目</a></li>
+		                <li style="padding: 0 0 4px 20px;"><a href="/zh/education/library_catalog_2014-04-05_toweb.xlsx">書目鏈接</a></li>
 		            </ul>
                 </li>
 	        </ul>
