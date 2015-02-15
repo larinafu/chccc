fa9d990fddbd648c7038dc329b1fe539661c98b3
yinghuihu <yinghuihu@gmail.com>
2015-02-15 08:16:26 -0500

    change the english calendar url

--- chccc/WebContent/en/content/event_content.php
+++ chccc/WebContent/en/content/event_content.php
@@ -1,4 +1,4 @@
 <div class="content">
-<iframe src="https://www.google.com/calendar/embed?src=ulr4lecoe4le2a35flk17qf3tc%40group.calendar.google.com&ctz=America/New_York"      
+<iframe src="https://www.google.com/calendar/embed?src=n0fnoqvoh0b5r6ejk6cj0jv6oc%40group.calendar.google.com&ctz=America/New_York"      
  style=" border-width:0 " width="930" height="600" frameborder="0"scrolling="no"></iframe> 
 </div>
