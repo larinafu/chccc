9ce50041498f7438846719a6efe8c1674a1bcfbc
yinghuihu <yinghuihu@gmail.com>
2015-06-03 20:07:42 -0400

    remove the obsolete links.

--- chccc/WebContent/zh/common/footer.php
+++ chccc/WebContent/zh/common/footer.php
@@ -4,13 +4,12 @@
 	  <ul class="quicklinks">
 	  	<li class="left">快速連接：</li>
 	  	<li class="left"><a href="/zh/quicklinks/neighbour_churches.php">周邊教會</a></li>
-	  	<li class="left"><a href="/zh/quicklinks/testimonies.php">見證如雲</a></li>
 	  	<li class="left"><a href="/zh/quicklinks/yearbooks.php">年刊一覽</a></li>
 	  	<li class="left"><a href="/zh/quicklinks/daily_bible.php">每日讀經</a></li>
 	  	<li class="left"><a href="/zh/quicklinks/english_learning.php">英語進修</a></li>
 	  	<li class="left"><a href="http://www.christianstudy.com/lifehymns.html">生命聖詩</a></li>
 	  	<li class="left"><a href="http://www.mediayoulike.com">聖詩在線</a></li>	  	
-	  	<li class="left"><a href="http://cherryhillccc.org/chinese/index.htm">舊址</a></li>
+	  	
 	  </ul>
   </div>
     </footer>
