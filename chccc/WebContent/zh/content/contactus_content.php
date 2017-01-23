b8874bfbafb5f8bb6a35f7c6bfe62f7b8804847d
yinghuihu <yinghuihu@gmail.com>
2017-01-23 06:00:37 -0500

    2017 contact update - chinese

--- chccc/WebContent/zh/content/contactus_content.php
+++ chccc/WebContent/zh/content/contactus_content.php
@@ -2,18 +2,21 @@
 <table id="contactus">
 <tr><td width="14%">李紹沅</td><td>牧師</td><td>pastorstevenlee@yahoo.com</td><td>856-428-1398</td></tr>
 <tr><td>滕靖</td><td>執事會主席</td><td>tengj6@yahoo.com</td><td>856-429-2106</td></tr>
-<tr><td>方家義</td><td>財務</td><td>jfang99@yahoo.com</td><td>631-590-1098</td></tr>
-<tr><td>鍾禮皋</td><td>敬拜/文書</td><td>pchong02@temple.edu</td><td>856-751-9428</td></tr>
+<tr><td>楊華清</td><td>財務</td><td>kevinyang1@gmail.com</td><td>405-473-3572</td></tr>
+<tr><td>鍾禮皋</td><td>敬拜</td><td>pchong02@temple.edu</td><td>856-751-9428</td></tr>
 <tr><td>張劉芳</td><td>教育</td><td>liufangzhang@gmail.com</td><td>609-926-8974</td></tr>
-<tr><td>馮衡生</td><td>英語/青少年</td><td>adam_feng@yahoo.com</td><td>856-267-5037</td></tr>
-<tr><td>劉亞霖</td><td>宣道</td><td>lzyah@yahoo.com</td><td>610-457-0227</td></tr>
+<tr><td>陳文聰	</td><td>英語/青少年</td><td>dannychccc@gmail.com</td><td>267-288-7169</td></tr>
+<tr><td>劉亞霖</td><td>宣道</td><td>yalinliu@hotmail.com</td><td>215-327-7641</td></tr>
+
 <tr><td>滕靖</td><td>團契</td><td>tengj6@yahoo.com</td><td>856-429-2106</td></tr>
-<tr><td>徐雷</td><td>會堂維修/總務</td><td>leejxu@yahoo.com</td><td>609-304-1878</td></tr>
+<tr><td>田永哲</td><td>會堂維修/總務</td><td>tianlinda@gmail.com</td><td>210-831-0781</td></tr>
 <tr><td>傅頡</td><td>環境維護</td><td>jeff3203100@yahoo.com</td><td>215-776-0416</td></tr>
-<tr><td>吳錫民</td><td>資訊/會員</td><td>benwu88@yahoo.com</td><td>856-938-3932</td></tr>
+<tr><td>吳錫民</td><td>資訊</td><td>benwu88@yahoo.com</td><td>856-938-3932</td></tr>
 <tr><td>吳冀川</td><td>關懷</td><td>jichuanwmail@gmail.com</td><td>856-778-1669</td></tr>
-<tr><td>楊華清</td><td>兒童</td><td>kevinyang1@gmail.com</td><td>405-473-3572</td></tr>
+<tr><td>李安雄</td><td>兒童</td><td>alee@scsmusic.com</td><td>856-220-6629</td></tr>
 <tr><td>朱晨</td><td>接待</td><td>zhuc25@hotmail.com</td><td>856-354-6016</td></tr>
+<tr><td>江懷德</td><td>文書/會員</td><td>jiang0167@yahoo.com</td><td>856-238-1958</td></tr>
+
 </table>
 </div>
 
