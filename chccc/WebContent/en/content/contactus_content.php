38b78a0c47f57515c779084de30e7e1a3b6ee767
yinghuihu <yinghuihu@gmail.com>
2017-01-30 07:35:25 -0500

    contact page updated.

--- chccc/WebContent/en/content/contactus_content.php
+++ chccc/WebContent/en/content/contactus_content.php
@@ -1,13 +1,33 @@
 <div class="subcontent">
 <table id="contactus">
-<tr><td>Rev. Muh-Chieh Yu</td><td>Senior Pastor</td><td>myu@cherryhillccc.org</td><td>856-428-1398</td></tr>
-<tr><td>Brother Hengshen Feng</td><td>Chairman of Deacon Board/Education/English/Teenager</td><td>adam_feng@yahoo.com</td><td>856-267-5037</td></tr>
-<tr><td>Brother Jiayi Fang</td><td>Finance/Writer</td><td>jfang99@yahoo.com</td><td>631-590-1098</td></tr>
-<tr><td>Brother Gongyao Zhou</td><td>Worship</td><td>zhoug@drexel.edu</td><td>856-751-9798</td></tr>
-<tr><td>Brother Huaqing Yang</td><td>Member/Environmental Protection</td><td>kevinyang1@gmail.com</td><td>405-473-3572</td></tr>
-<tr><td>Brother Lei Xu</td><td>General Affairs</td><td>leejxu@yahoo.com</td><td>856-768-8648</td></tr>
-<tr><td>Brother Xiaocang Zhao</td><td>Mission/Fellowship/Care</td><td>alvinhtchao@hotmail.com</td><td>856-264-5396</td></tr>
-<tr><td>Brother Minhu Hu</td><td>Building Maintanance</td><td>hupaul@comcast.net</td><td>856-489-9130</td></tr>
-<tr><td>Brother Yu Cao</td><td>Children/Information</td><td>simoncao@yahoo.com</td><td>856-780-5190</td></tr>
+<tr><td>Steven Lee</td><td>Pastor</td><td>pastorstevenlee@yahoo.com</td><td>856-428-1398</td></tr>
+<tr><td>Jing Teng</td><td  style="max-width:150px;">Chair person / Fellowship</td><td>tengj6@yahoo.com</td><td>856-429-2106</td></tr>
+<tr><td>Kevin Yang</td><td>Treasurer</td><td>kevinyang1@gmail.com</td><td>405-473-3572</td></tr>
+<tr><td>Lee-Gau Chong</td><td>Worship</td><td>pchong02@temple.edu</td><td>856-751-9428</td></tr>
+<tr><td>Liufang Zhang</td><td>Education</td><td>liufangzhang@gmail.com</td><td>609-926-8974</td></tr>
+<tr><td>Danny Chan</td><td  style="max-width:150px;">English/Youth Ministry</td><td>dannychccc@gmail.com</td><td>267-288-7169</td></tr>
+<tr><td>Yalin Liu</td><td>Mission</td><td>yalinliu@hotmail.com</td><td>215-327-7641</td></tr>
+<tr><td>Yongzhe Tian</td><td  style="max-width:150px;">Building /General Affairs</td><td>tianlinda@gmail.com</td><td>210-831-0781</td></tr>
+<tr><td>Jie Fu</td><td  style="max-width:150px;">Facility Maintainance</td><td>jeff3203100@yahoo.com</td><td>215-776-0416</td></tr>
+<tr><td>Ben Wu</td><td>Information</td><td>benwu88@yahoo.com</td><td>856-938-3932</td></tr>
+<tr><td>Jichuan Wu</td><td>Caring</td><td>jichuanwmail@gmail.com</td><td>856-778-1669</td></tr>
+<tr><td>Andrew Lee</td><td  style="max-width:150px;">Children Ministry</td><td>andrewohlee@gmail.com</td><td>856-220-6629</td></tr>
+<tr><td>Chen Zhu</td><td>Hospitality</td><td>zhuc25@hotmail.com</td><td>856-354-6016</td></tr>
+<tr><td>Huaide Jiang</td><td>Clerk/Member</td><td>jiang0167@yahoo.com</td><td>856-238-1958</td></tr>
 </table>
+
+
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+
 </div>
