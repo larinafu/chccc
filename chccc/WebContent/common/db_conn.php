58e81f730d50d41f9ceb9be1977b1b837f3667e0
yinghuihu <yinghuihu@gmail.com>
2017-01-31 09:48:12 -0500

    add the variables for the devotion messages.

--- chccc/WebContent/common/db_conn.php
+++ chccc/WebContent/common/db_conn.php
@@ -4,7 +4,9 @@ $password="53chccc2004";//"Uvfy5RYduLj8fEMa"; //
 $database="chccc";
 $db_host="localhost"; //;//mysql
 $audio_library="http://www.cherryhillccc.org/ChineseSundayMessage/";
-$pdf_library="/ChineseSundayMessage/PDF/";	
+$pdf_library="/ChineseSundayMessage/PDF/";
+$devotion_audio_library="/DailyDevotionMessage/";
+$devotion_pdf_library="/DailyDevotionMessage/";
 $audio_library_en="http://www.cherryhillccc.org/ChineseSundayMessage/EnglishService/";
 $pdf_library_en="/ChineseSundayMessage/EnglishService/PDF/";		
 ?>
