15541c1bb5f4755438abd60094c15761103e52b7
Larina Fu <lrf59@cornell.edu>
2021-08-07 11:28:21 -0400

    formatted more correctly

--- new-church-redesign/src/Components/Navbar/Navbar.js
+++ new-church-redesign/src/Components/Navbar/Navbar.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import Navbar from "react-bootstrap/Navbar"
+import { Link } from "react-router-dom";
 
 const Navbar = (props) => {
-    return (
-        <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/about'>About</Link></li>
-            <li><Link to='/worship'>Worship</Link></li>
-            <li><Link to='/sermons'>Sermons</Link></li>
-        </ul>
-    )
-}
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/worship">Worship</Link>
+        </li>
+        <li>
+          <Link to="/sermons">Sermons</Link>
+        </li>
+      </ul>
+    </nav>
+    
+  );
+};
 
 export default Navbar;
