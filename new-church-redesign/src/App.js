489d102ee78a5dce61280e0923852e99f53616b3
Larina Fu <lrf59@cornell.edu>
2021-08-07 12:15:23 -0400

    added styling to navbar

--- new-church-redesign/src/App.js
+++ new-church-redesign/src/App.js
@@ -8,20 +8,20 @@ import About from "./Components/About";
 import Sermons from "./Components/Sermons";
 import Worship from "./Components/Worship";
 
+import { useState } from "react";
 import { Route } from "react-router-dom";
 
-
 function App() {
+  const [isEnglish, setIsEnglish] = useState(true);
   return (
     <div>
-      <Navbar />
+      <Navbar isEnglish={isEnglish} setIsEnglish={setIsEnglish} />
       <p>hello world</p>
       <p>My name is larina!</p>
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/sermons" component={Sermons} />
       <Route exact path="/worship" component={Worship} />
-
     </div>
   );
 }
--- new-church-redesign/src/Components/Navbar/Navbar.js
+++ new-church-redesign/src/Components/Navbar/Navbar.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Navbar.css";
+
 const Navbar = (props) => {
-  return (
+  const handleToggle = () => {
+    props.setIsEnglish(!props.isEnglish);
+  };
+  return props.isEnglish ? (
     <nav>
       <ul>
         <li>
@@ -18,8 +23,48 @@ const Navbar = (props) => {
           <Link to="/sermons">Sermons</Link>
         </li>
       </ul>
+      <div>
+        <button className="donateBtn">Donate</button>
+        <button
+          className={`toggleBtn ${
+            props.isEnglish ? "toggleEnglish" : "toggleChinese"
+          }`}
+          onClick={handleToggle}
+        >
+          <div>{props.isEnglish ? "EN" : "中文"}</div>
+          <div className="toggleCircle"></div>
+        </button>
+      </div>
     </nav>
-    
+  ) : (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">主頁</Link>
+        </li>
+        <li>
+          <Link to="/about">簡介</Link>
+        </li>
+        <li>
+          <Link to="/worship">崇拜</Link>
+        </li>
+        <li>
+          <Link to="/sermons">講道</Link>
+        </li>
+      </ul>
+      <div>
+        <button className="donateBtn">奉献</button>
+        <button
+          className={`toggleBtn ${
+            props.isEnglish ? "toggleEnglish" : "toggleChinese"
+          }`}
+          onClick={handleToggle}
+        >
+          <div>{props.isEnglish ? "EN" : "中文"}</div>
+          <div className="toggleCircle"></div>
+        </button>
+      </div>
+    </nav>
   );
 };
 
